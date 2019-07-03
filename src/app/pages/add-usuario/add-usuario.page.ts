d9a4a5532efcdf4c9981eeca39dfa9cd7414e495
THEVOL <169121742018.2@senacrjedu.local>
2019-07-03 09:29:46 -0300

    alert de salvar pronto 03/07/2019

--- src/app/pages/add-usuario/add-usuario.page.ts
+++ src/app/pages/add-usuario/add-usuario.page.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Usuario } from 'src/app/model/usuario';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-add-usuario',
   templateUrl: './add-usuario.page.html',
   styleUrls: ['./add-usuario.page.scss'],
 })
+//constructor(public alertController: AlertController) {}
 export class AddUsuarioPage implements OnInit {
   
   public usuario:Usuario;
 
-  constructor() { }
+  constructor(public alertController: AlertController, public router: Router) {}
 
   ngOnInit() {
     this.usuario = new Usuario;
   }
 
   onSubmit(form){
-      console.log("Cadastrado",this.usuario)
+    if (form.valid){
+      //console.log("Cadastrado",this.usuario);
+      this.presentAlert("Aviso","Cadastrado!")
+      form.reset;
+      this.router.navigate(['/'])
+    }
+}
+
+  async presentAlert(titulo:string,texto: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+     //subHeader:'',
+      message: texto,
+      buttons: ['Talkey']
+    });
+
+    await alert.present();
   }
 
 }
